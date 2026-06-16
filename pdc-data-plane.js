9cc4bb429f87b3103cfd81ef1ba625f975f1457c
Paul Libbrecht <paul@hoplahup.net>
2026-06-16 18:42:50 +0200

    fixes for PDC compatibility.

--- pdc-data-plane.js
+++ pdc-data-plane.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import {directories} from "./directory-authorizations.js"
 import jwt from 'jsonwebtoken'
-const {publicEncrypt} = await import('node:crypto');
 import * as child_process from "node:child_process";
 import fs from "node:fs"
 import path from "node:path"
@@ -21,20 +20,23 @@ server.listen(PORT, () => {
 
 // authorization route; delivers an authorization behind the PDC
 // this route should only be accessible from the server of your own PDC.
+let requestCounter = 0;
 const auth = (req,res)=> {
-  console.log("Received req ", req)
-  const params = req.body || req.query
-  console.log((new Date().toISOString()) + " Received auth ", JSON.stringify(params))
+  requestCounter += 1;
+  console.log((requestCounter + " " + Date()) + " Received request from client \"" + req.header("user-agent") + "\" at address "+ req.header("x-forwarded-for")+".");
+  let params = req.body || req.query
+  if(params.data) // ignore that thing, we don't handle "data"
+    params = req.query;
+  console.log(requestCounter + " " + " Received /auth ", JSON.stringify(params))
   try {
-    const url = process.env.BASE_URL + "/get/" + escape(params.resource)
-    const pubkeyText = params.pubkey, signatureBase64 = params.signature
+    const url = process.env.BASE_URL + "/get/" + encodeURIComponent(params.resource)
+    let pubkeyText = params.pubkey, signatureBase64 = params.signature
+    pubkeyText = Buffer.from(pubkeyText,'base64').toString('utf-8');
+    if(!pubkeyText.endsWith("\n")) pubkeyText = pubkeyText + "\n"
     if (!params || !pubkeyText || !params.resource || !signatureBase64) res.status(500).send("No valid body received (need pubkey, resource and signature")
     const resource = params.resource, pubkey = Buffer.from(pubkeyText),
       signature = Buffer.from(signatureBase64.trim(), 'base64');
 
-    // verify signature for the string resource=<resource>&pubkey=<pubkey> in a random dir
-    const dir = "tmp/" + Math.random().toString(36).substr(5);
-    fs.mkdirSync(dir, { recursive: true });
 
     // Two alternative message formats to be signed
     const msgs =
@@ -42,29 +44,35 @@ const auth = (req,res)=> {
       [`resource=${encodeURIComponent(resource)}&pubkey=${encodeURIComponent(pubkeyText)}`,
         // as JSON object
         JSON.stringify({pubkey:pubkeyText, resource: resource})];
-    let passed = false, verification = "", i=0;
+    let passed = false, verification = "", i=0, error = null;
     for(let msg of msgs){
+      // verify signature for the string resource=<resource>&pubkey=<pubkey> in a random dir
+      const dir = "tmp/" + Math.random().toString(36).substring(5);
+      fs.mkdirSync(dir, { recursive: true });
       try { // from https://pagefault.blog/2019/04/22/how-to-sign-and-verify-using-openssl/
         // openssl dgst -verify key.pub -keyform PEM -sha256 -signature file.sign -binary file
         fs.writeFileSync(path.resolve(dir, "key.pub"), pubkey);
         fs.writeFileSync(path.resolve(dir, "signature.sign"), signature);
         fs.writeFileSync(path.resolve(dir, "messagefile.txt"), msg, {encoding: "utf8"});
-        console.log("Will verify signature for message " + i++ + " in " + dir)
+        console.log(requestCounter + " "+"Will verify signature for message " + i++ + " in " + dir)
         verification = child_process.execSync(`openssl dgst -verify key.pub -keyform PEM -sha256 -signature signature.sign -binary messagefile.txt`,
           {timeout: 10000, windowsHide: true, cwd: dir}).toString()
         //fs.rmdirSync(dir)
         passed = "Verified OK" === verification.trim();
-        console.log("Verification passed ? " + passed );
+        console.log(requestCounter + " " +"Verification passed ? " + passed );
         if (passed)  break;
       } catch (e) {
-        console.error(e);
+        console.info(requestCounter + " " + " Error: " + e);
+        console.info(e);
+        error = e;
       }
     }
     if(!passed) {
-      res.status(500).send("Signature verification failed: " + verification);
-      throw new Error("Signature verification failed: " + verification)
+      if(!error) error = "Signature verification failed: " + verification;
+      res.status(500).send(error);
+      return;
     }
-    console.log("Signature verified, creating token.")
+    console.log((requestCounter + " ") +"Signature verified, creating token.")
     const expiry = Math.ceil(Date.now() / 1000) + 3600 * 8, // 2h
       payload = {
         url: url,
@@ -75,14 +83,19 @@ const auth = (req,res)=> {
     if (directories.checkAuth(resource, pubkeyText)) {
       const token = jwt.sign(payload, privKey, {algorithm: 'RS256'});
       res.setHeader('content-type', 'text/plain');
-      console.log("Responding token ", token)
+      console.log((requestCounter + " ") +"Responding token ", token)
       res.send(token)
     } else {
-      console.log("Received authorization wrong.")
+      console.log((requestCounter + " ") + "Received authorization wrong.")
       res.status(500).send("Authorization error")
+      return;
     }
   } catch (e) {
-    console.warn("error", e)
+    if(e.toString().startsWith("Error: ENOENT: no such file or directory")) {
+      console.log((requestCounter + " ") + " no such file or directory " + e)
+      res.status(404).send("No such file or directory \"" + params.resource + "\"."); return;
+    }
+    console.log((requestCounter + " ") + " error", e)
     res.status(500).send("Error " + JSON.stringify(e))
   }
 }
@@ -96,7 +109,7 @@ server.get("/", (req,res) => {
   res.status(200).send("This is the PDC data plane. Please see the source. https://github.com/Cabri/pdc-data-plane")
 });
 
-server.get("/get/:claimedResource", (req,res, resource) => {
+server.get("/get/:claimedResource", (req,res) => {
   const claimedResource = req.params.claimedResource
   console.log("getting " + claimedResource);
   const authHeader = req.header('authorization')
